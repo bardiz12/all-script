09a3520ae719e2af96646686f09ca5238dc214f7
Bard <>
2019-11-18 22:51:19 +0700

    add Heroku Environtment Setter

---
+++ heroku-env-setter/sendENV.py
@@ -0,0 +1,19 @@
+import os,sys
+
+
+if len(sys.argv) < 1 :
+    print("usage: python3 sendENV.py .env_file")
+    exit()
+
+
+env = open(sys.argv[1],'r').read()
+envs = env.splitlines()
+
+for env in envs:
+    parsed = env.split("=",1)
+    if(len(parsed) > 1):
+        key = parsed[0]
+        value = parsed[1]
+        if(value != ""):
+            print(env)
+            os.system("heroku config:set " + env)
